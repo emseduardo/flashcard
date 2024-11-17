3f2a3f91afe445fff7e54ec511a75b3246087583
emseduardo <00001108422895sp@al.educacao.sp.gov.br>
2024-11-16 21:17:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual foi a importância da Revolução Industrial para o desenvolvimento das sociedades modernas?',
+    'A Revolução Industrial foi importante porque acelerou a produção de bens, impulsionou o desenvolvimento tecnológico e urbano, além de transformar as relações de trabalho e a economia global.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a diferença entre uma equação linear e uma equação quadrática?',
+    'Uma equação linear tem grau 1 e sua representação gráfica é uma linha reta, enquanto uma equação quadrática tem grau 2 e sua representação gráfica é uma parábola.'
+)
+
+criaCartao(
+    'Química',
+    'Por que o pH é uma medida importante em soluções químicas?',
+    'O pH é importante porque indica a acidez ou basicidade de uma solução, o que é essencial para controlar reações químicas e manter condições adequadas em processos biológicos e industriais.'
+)
+
+criaCartao(
+    'Geografia',
+    'Por que o clima tropical é caracterizado por altas temperaturas e chuvas frequentes?',
+    'O clima tropical tem altas temperaturas e chuvas frequentes devido à proximidade da região ao Equador, onde há maior incidência solar e intensa evaporação, resultando em altos índices de precipitação.'
+)
